710e8cf6b2969ba64d6953d532984e1ddecbfd4b
root <ngohuynhngockhanh@gmail.com>
2018-02-19 15:06:18 +0700

    please login addon

--- index.js
+++ index.js
@@ -7,15 +7,13 @@ const socketServerURL = 'http://socket.mysmarthome.vn'
 var socket = socket_client(socketServerURL); 
 
 socket.on('connect', function() {
+})
+
+socket.on("please_login", function() {
 	socket.emit('register_node', {
 		"node_id":"r1O92dtSz",
 		"uuid":"PsxUPldlbBNEuEUTi7Ci5yls77R2",
 		"token":"301fd2e614a97927804613e73373ded2991661182107"
-	})
-	socket.emit('register_node', {
-		"node_id":"BJg3q5Yrz",
-		"uuid":"BHNOrSlw1qhIHuyWk8SRzS1oncu1",
-		"token":"cf91d9b4243c5fa18808fdf3d6e681c3358767364533"
 	})
 })
 
